0602aaf6dc57248146d0b3561b5f78dfd8275454
Warlen <warlensa007@gmail.com>
2025-01-17 19:32:17 -0300

    feat: creating login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
-import { FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormRecord, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+import { ToastrService } from 'ngx-toastr';
 
+interface LoginForm {
+  email: FormControl,
+  password: FormControl
+}
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     DefaultLoginLayoutComponent,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    PrimaryInputComponent
+  ],
+  providers:[
+    LoginService
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
@@ -15,12 +27,27 @@ import { FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 export class LoginComponent {
   loginForm: FormGroup;
 
-  constructor(){
-    this.loginForm = new FormGroup({
-      email: new FormGroup('',[Validators.required]),
-      password: new FormGroup('',[Validators.required, Validators.minLength(6)])
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+    private toastService: ToastrService
 
+  ){
+    this.loginForm = new FormGroup({
+      email: new FormControl('',[Validators.required]),
+      password: new FormControl('',[Validators.required, Validators.minLength(6)])
     })
+  }
+
+  submit(){
+    this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      next: () => this.toastService.success("Login feito com sucesso!"),
+      error: () => this.toastService.error("Erro inesperado! Tente novamente mais tarde"),
+    })
+  }
+
+  navigate(){
+    this.router.navigate(["/signup"])
   }
 
 }
